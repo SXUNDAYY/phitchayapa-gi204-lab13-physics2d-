de06edc1f01f6909a481f4094a053c276357f54e
Phitchayapa Chuchai <phitchayapa.chuc@bumail.net>
2026-04-24 18:51:57 +0700

    add-physicsmaterial

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -24,7 +24,7 @@ public class PlayerMovement : MonoBehaviour
         rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y);
 
         //jump
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !isJumping)
         {
             rb2d.AddForce(new Vector2 (rb2d.linearVelocity.x, jumpForce));
             Debug.Log("Jump");
